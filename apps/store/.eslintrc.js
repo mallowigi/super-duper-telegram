3ba2cc60c6063859f6ae7fe4821d93c226efd22c
Elior <heliosaian@gmail.com>
2022-07-01 18:04:17 +0300

    chore(project): use mantine appstate and fix eslintrc

---
+++ apps/store/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['plugin:@nrwl/nx/react', '../../.eslintrc.js'],
+  ignorePatterns: ['!**/*'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+      rules: {},
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {},
+    },
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {},
+    },
+  ],
+};
--- apps/store/src/app/App.tsx
+++ apps/store/src/app/App.tsx
@@ -1,13 +1,87 @@
-import { MantineProvider } from "@mantine/core";
+import {
+  AppShell,
+  Header,
+  MantineProvider,
+  Navbar,
+  useMantineTheme,
+  Text,
+  Footer,
+  MediaQuery,
+  Aside,
+  Burger
+} from "@mantine/core";
+import { useState, useCallback } from "react";
 
-export const App = () => (
-  <MantineProvider
-    theme={{
-      // Override any other properties from default theme
-      fontFamily: "Open Sans, sans serif",
-      spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 }
-    }}
-  >
-    <div className="app">Hi</div>
-  </MantineProvider>
-);
+export const App = () => {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+
+  const toggle = useCallback(() => {
+    setOpened(!opened);
+  }, [opened]);
+
+  return (
+    <MantineProvider
+      theme={{
+        colorScheme: "light"
+      }}
+    >
+      <AppShell
+        aside={
+          <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+            <Aside hiddenBreakpoint="sm" p="md" width={{ lg: 300, sm: 200 }}>
+              <Text>Application sidebar</Text>
+            </Aside>
+          </MediaQuery>
+        }
+        asideOffsetBreakpoint="sm"
+        fixed
+        footer={
+          <Footer height={60} p="md">
+            Application footer
+          </Footer>
+        }
+        header={
+          <Header height={70} p="md">
+            <div
+              style={{ alignItems: "center", display: "flex", height: "100%" }}
+            >
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger
+                  color={theme.colors.gray[6]}
+                  mr="xl"
+                  onClick={toggle}
+                  opened={opened}
+                  size="sm"
+                />
+              </MediaQuery>
+
+              <Text>Application header</Text>
+            </div>
+          </Header>
+        }
+        navbar={
+          <Navbar
+            hidden={!opened}
+            hiddenBreakpoint="sm"
+            p="md"
+            width={{ lg: 300, sm: 200 }}
+          >
+            <Text>Application navbar</Text>
+          </Navbar>
+        }
+        navbarOffsetBreakpoint="sm"
+        styles={{
+          main: {
+            background:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0]
+          }
+        }}
+      >
+        <Text>Resize app to see responsive navbar in action</Text>
+      </AppShell>
+    </MantineProvider>
+  );
+};
